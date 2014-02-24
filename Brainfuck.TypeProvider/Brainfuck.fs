640d317c2a42d374739cc464a000abcb8106e60a
Anthony Brown <bruinbrown@gmail.com>
2014-02-24 22:05:35 +0000

    Added it all

---
+++ Brainfuck.TypeProvider/Brainfuck.fs
@@ -0,0 +1,44 @@
+﻿namespace Brainfuck
+
+module Interpreter =
+    let FindJump dir (program:char []) cidx =
+        let s = System.Collections.Generic.Stack<int>()
+        let rec NextChar index =
+            match program.[index], dir with
+            | '[', 1
+            | ']', -1 -> s.Push(index)
+                         NextChar (index+dir)
+            | ']', 1
+            | '[', -1 -> if s.Count = 0 then (index+1)
+                         else
+                             s.Pop() |> ignore
+                             NextChar (index+dir)
+            | _ -> NextChar (index+dir)
+        NextChar (cidx+dir)
+    
+    let FindForwardJump = FindJump 1
+    
+    let FindBackwardJump = FindJump -1
+
+    let rec Execute (program:string) ip dp (data:int []) output =
+        if ip = program.Length then data,output
+        else
+            match program.[ip] with
+            | '>' -> Execute program (ip+1) (dp+1) data output
+            | '<' -> Execute program (ip+1) (dp-1) data output
+            | ',' -> let k = System.Console.ReadKey().KeyChar |> int
+                     data.[dp] <- k
+                     Execute program (ip+1) dp data output
+            | '.' -> let o =  output + sprintf "%c" (data.[dp] |> char)
+                     Execute program (ip+1) dp data o
+            | '+' -> do data.[dp] <- data.[dp] + 1
+                     Execute program (ip+1) dp data output
+            | '-' -> do data.[dp] <- data.[dp] - 1
+                     Execute program (ip+1) dp data output
+            | '[' -> let idx = if data.[dp] = 0 then (FindForwardJump (program.ToCharArray()) ip) else (ip+1)
+                     Execute program idx dp data output
+            | ']' -> let idx = if data.[dp] <> 0 then (FindBackwardJump (program.ToCharArray()) ip) else (ip+1)
+                     Execute program idx dp data output
+            | _ -> Execute program (ip+1) dp data output
+    
+    let Run program memorySize = Execute program 0 0 (Array.zeroCreate memorySize) ""
---
+++ Brainfuck.TypeProvider/BrainfuckProvider.fs
@@ -0,0 +1,44 @@
+﻿namespace Brainfuck
+
+open ProviderImplementation.ProvidedTypes
+open Microsoft.FSharp.Core.CompilerServices
+open System.Reflection
+open Brainfuck.Interpreter
+
+[<TypeProvider>]
+type BrainfuckTypeProvider (cfg:TypeProviderConfig) as this =
+    inherit TypeProviderForNamespaces()
+
+    let asm = Assembly.GetExecutingAssembly()
+    let ns = "Brainfuck.TypeProvider"
+    let baseType = typeof<obj>
+    let staticParams = [ProvidedStaticParameter("program", typeof<string>)
+                        ProvidedStaticParameter("memorySize", typeof<int>, parameterDefaultValue = 512)]
+
+    let runnerType = ProvidedTypeDefinition(asm, ns, "BrainfuckProvider", Some baseType)
+
+    do runnerType.DefineStaticParameters(
+        parameters = staticParams,
+        instantiationFunction = (fun typeName parameterValues ->
+            match parameterValues with
+            | [| :? string as program; :? int as memorySize |]
+                -> let memory, output = Run program memorySize
+                   let consoleOutput = ProvidedProperty("ConsoleOutput", typeof<string>, IsStatic = true, GetterCode = fun args -> <@@ output @@>)
+                   let memoryType = ProvidedTypeDefinition("Memory", Some typeof<obj>)
+                   let memoryProperties = memory
+                                          |> Seq.mapi (fun i t -> ProvidedProperty(i.ToString(), typeof<int>, IsStatic = true,
+                                                                                   GetterCode = fun args -> <@@ t @@>))
+                                          |> List.ofSeq
+
+                   let ty = ProvidedTypeDefinition(asm, ns, typeName, Some typeof<obj>)
+
+                   do memoryType.AddMembers(memoryProperties)
+                   do ty.AddMember(consoleOutput)
+                   do ty.AddMember(memoryType)
+                   ty
+            | _ -> failwith "Unexpected number of parameters"))
+
+    do this.AddNamespace(ns, [runnerType])
+
+[<assembly:TypeProviderAssembly>]
+do ()
