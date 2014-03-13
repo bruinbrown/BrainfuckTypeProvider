053cf9bc78720c9e58b002a65dd31a273ab2bb93
Anthony Brown <bruinbrown@gmail.com>
2014-03-13 23:18:42 +0000

    Added support for chained programs with an infinite type system

--- Brainfuck.TypeProvider/BrainfuckProvider.fs
+++ Brainfuck.TypeProvider/BrainfuckProvider.fs
@@ -12,29 +12,68 @@ type BrainfuckTypeProvider (cfg:TypeProviderConfig) as this =
     let asm = Assembly.GetExecutingAssembly()
     let ns = "Brainfuck.TypeProvider"
     let baseType = typeof<obj>
-    let staticParams = [ProvidedStaticParameter("program", typeof<string>)
+    let staticParams = [ProvidedStaticParameter("program", typeof<string>, parameterDefaultValue = "")
+                        ProvidedStaticParameter("input", typeof<string>, parameterDefaultValue = "")
                         ProvidedStaticParameter("memorySize", typeof<int>, parameterDefaultValue = 512)]
 
     let runnerType = ProvidedTypeDefinition(asm, ns, "BrainfuckProvider", Some baseType)
+
+    let EvaluateProgramToProperty program =
+        program
+        |> RunWithoutInput
+        |> snd
+        |> fun t -> ProvidedProperty("ConsoleOuput",
+                                     typeof<string>,
+                                     IsStatic = true,
+                                     GetterCode = fun args -> <@@ t @@>)
+
+    let CreateErrorProperty () =
+        let q = ProvidedProperty("Invalid Program",
+                                 typeof<string>,
+                                 IsStatic = true,
+                                 GetterCode = fun args -> <@@ "Invalid program" @@>)
+        q.AddXmlDoc("The interpreter found an error in your program so far. Please correct it.")
+        q
+
+    let LazyEvaluate program =
+        match CheckValidity program with
+        | Valid -> EvaluateProgramToProperty program
+        | _ -> CreateErrorProperty ()
+
+    let rec createTypes prog =
+        let operators = ["<"; ">"; ",";".";"-";"+";"[";"]"]
+        let typs = operators
+                   |> List.map (fun t -> let newProg = prog + t
+                                         let programProperty = ProvidedProperty("Program", 
+                                                                                typeof<string>,
+                                                                                IsStatic = true,
+                                                                                GetterCode = fun args -> <@@ newProg @@>)
+                                         let s = ProvidedTypeDefinition(t, Some typeof<obj>)
+                                         s.AddMembersDelayed(fun () -> createTypes newProg)
+                                         s.AddMemberDelayed(fun () -> LazyEvaluate newProg)
+                                         s.AddMember(programProperty)
+                                         s)
+        typs
 
     do runnerType.DefineStaticParameters(
         parameters = staticParams,
         instantiationFunction = (fun typeName parameterValues ->
             match parameterValues with
-            | [| :? string as program; :? int as memorySize |]
-                -> let memory, output = Run program memorySize
-                   let consoleOutput = ProvidedProperty("ConsoleOutput", typeof<string>, IsStatic = true, GetterCode = fun args -> <@@ output @@>)
-                   let memoryType = ProvidedTypeDefinition("Memory", Some typeof<obj>)
-                   let memoryProperties = memory
-                                          |> Seq.mapi (fun i t -> ProvidedProperty(i.ToString(), typeof<int>, IsStatic = true,
-                                                                                   GetterCode = fun args -> <@@ t @@>))
-                                          |> List.ofSeq
+            | [| :? string as program; :? string as input; :? int as memorySize |]
+                -> let memory, output = Run program memorySize input
+                   let consoleOutput = ProvidedProperty("ConsoleOutput", 
+                                                        typeof<string>,
+                                                        IsStatic = true,
+                                                        GetterCode = fun args -> <@@ output @@>)
 
+                   let programProperty = ProvidedProperty("Program",
+                                                          typeof<string>,
+                                                          IsStatic = true,
+                                                          GetterCode = fun args -> <@@ program @@>)
                    let ty = ProvidedTypeDefinition(asm, ns, typeName, Some typeof<obj>)
-
-                   do memoryType.AddMembers(memoryProperties)
-                   do ty.AddMember(consoleOutput)
-                   do ty.AddMember(memoryType)
+                   ty.AddMembersDelayed(fun () -> createTypes program)
+                   ty.AddMember(consoleOutput)
+                   ty.AddMember(programProperty)
                    ty
             | _ -> failwith "Unexpected number of parameters"))
 
